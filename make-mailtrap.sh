c85f34a60323d7aed6e12080906d78feb70f4d76
drupalpro <drupalpro@quickstart>
2013-05-13 06:19:45 -0700

    Added a make-mailtrap.sh

---
+++ make-mailtrap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo dpkg --purge postfix
+sudo apt-get install postfix
+
+PF=`cat /etc/postfix/main.cf | grep sasl_passwd`
+
+if [ -z "$PF"]; then
+	 echo smtp_sasl_auth_enable = yes | sudo tee -a /etc/postfix/main.cf
+	 echo smtp_sasl_mechanism_filter = plain | sudo tee -a /etc/postfix/main.cf
+	 echo smtp_sasl_security_options = noanonymous | sudo tee -a /etc/postfix/main.cf
+	 echo smtp_sasl_password_maps = hash:/etc/postfix/sasl_passwd | sudo tee -a /etc/postfix/main.cf
+fi
+
+echo "mailtrap.io localdev-3436c605e37cc25e:82661e54bea5d52f" | sudo tee /etc/postfix/sasl_passwd
+
+sudo postmap /etc/postfix/sasl_passwd
+sudo /etc/init.d/postfix restart

e7aad806f3b43eceb12d19a003022a73b483c050
drupalpro <drupalpro@quickstart>
2013-05-28 23:54:08 -0700

    Changed make to work for quick start

--- make.sh
+++ make.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-URL="https://raw.github.com/perceptum/dd_ecommerce_publix/master/dd-dev.v.0.2.make"
-wget $URL -v -O /tmp/dd-dev.make
-drush quickstart-create --domain=ddeals.dev --makefile=/tmp/dd-dev.make --profile=daddy_s_deals
-rm /tmp/dd-dev.make
+
+MAKEFILE_URL="https://raw.github.com/perceptum/dd_ecommerce_publix/master/dd-dev.v.0.1.make"
+MAKE_FILE_DL=/tmp/dd-dev.`date +%s`.make
+
+rm -f $MAKE_FILE_DL
+
+echo "Downloading make file"
+wget $MAKEFILE_URL -v -O $MAKE_FILE_DL
+if [ ! -f "$MAKE_FILE_DL" ]; then
+  echo "$MAKEFILE_URL could not be downloaded"
+fi;
+
+echo "building new quickstart site"
+drush quickstart-create --domain=ddeals.dev --makefile=$MAKE_FILE_DL --profile=daddy_s_deals
+
+echo "Cleaning up ..."
+rm -f $MAKE_FILE_DL
+echo "done"
